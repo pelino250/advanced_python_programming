45ca018625d5563faf71ae06a69627aabd615b36
pelino250 <pmutanguha@alueducation.com>
2024-09-06 16:11:43 +0200

    update week_one

--- week_one/annotations.py
+++ week_one/annotations.py
@@ -43,10 +43,10 @@ def repeat_string(s: str, n: int) -> str:
     return s * n
 
 
-try:
-    print(repeat_string(3, '3'))  # Raises ValueError
-except ValueError as e:
-    print(e)
+# try:
+#     print(repeat_string(3, '3'))  # Raises ValueError
+# except ValueError as e:
+#     print(e)
 
 
 # def test_func(a:str):
@@ -67,3 +67,23 @@ except ValueError as e:
 # 4 - dependency injection / framework:
 
 # 5 - serialization / deserialization
+
+def demo_argument(name, email, age=18):
+    pass
+
+@validate
+def demo_argument_annotated(name: str, email: str, age: int = 18) -> None:
+    pass
+
+def demo_argument2(*args):
+    pass
+
+def demo_argument3(**kwargs):
+    pass
+
+def demo_argument4(name, *args, **kwargs):
+    pass
+
+demo_argument_annotated("test", "Pelin", age=20)
+# demo_argument2("Pelin", 20, "test", 2, 5, 6, 7, 8, 9, 10)
+# demo_argument3(name="test", age=20, test="test")
